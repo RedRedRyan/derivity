02e05fdea5641161f4814257ac84351e1de0d139
RedRedRyan <ryankipkorir2002@gmail.com>
2026-09-02 07:33:39 +0300

    authkey

--- src/components/shared/utils/config/config.ts
+++ src/components/shared/utils/config/config.ts
@@ -304,7 +304,7 @@ export const DOMAIN_CONFIG: Record<string, DomainConfig> = {
         primaryDomain: 'cyctrades.vercel.app',
         aliases: ['www.cyctrades.vercel.app'],
         clientId: process.env.CLIENT_ID || '33PPqCmoCqt9yWsbBuSPu',
-        appId: process.env.APP_ID || '33PPqCmoCqt9yWsbBuSPu',
+        appId: process.env.APP_ID || '',
         redirectUri: process.env.REDIRECT_URI || 'https://cyctrades.vercel.app/',
         includeLegacyAppIdInOAuth: true,
         features: {
@@ -326,7 +326,7 @@ export const DOMAIN_CONFIG: Record<string, DomainConfig> = {
         primaryDomain: 'cyctrades.vercel.app',
         aliases: ['www.cyctrades.vercel.app'],
         clientId: '33PPqCmoCqt9yWsbBuSPu',
-        appId: '122208',
+        appId: '',
         redirectUri: 'https://cyctrades.vercel.app/',
         botsFolder: 'optimumtraders.site',
         includeLegacyAppIdInOAuth: false,
